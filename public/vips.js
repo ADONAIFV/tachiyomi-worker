fc46a0e902a678f8eb64ed9f6a606c2519c5a5ae
ADONAIFV <166330966+ADONAIFV@users.noreply.github.com>
2026-01-19 18:18:17 -0500

    Delete public/vips.js

--- public/vips.js
+++
@@ -1,3 +0,0 @@
-// Este es el contenido para public/vips.js
-var vips;vips=function(){var _scriptDir=import.meta.url;return function(vips_module){/* ... (el código minificado de vips.js) ... */}}();
-export default vips;
